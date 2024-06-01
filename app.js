75872408e5204704dd88dc7c980dcfc22244f7cb
Balla Kavya <ballakavya594@gmail.com>
2024-06-01 15:54:51 +0530

    Update

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 function appendToDisplay(value) {
-    document.getElementById("display").value += value;
+  document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+  document.getElementById('display').value = '';
+}
+
+function calculate() {
+  try {
+      const display = document.getElementById('display');
+      display.value = eval(display.value);
+  } catch (error) {
+      alert('Invalid Expression');
   }
-  
-  function calculate() {
-    let expression = document.getElementById("display").value;
-    let result;
-  
-    try {
-      result = eval(expression);
-    } catch (error) {
-      result = "Error";
-    }
-  
-    document.getElementById("display").value = result;
-  }
-  
-  function clearDisplay() {
-    document.getElementById("display").value = "";
-  }
+}
+
+function backspace() {
+  const display = document.getElementById('display');
+  display.value = display.value.slice(0, -1);
+}
